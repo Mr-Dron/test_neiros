9e2eb10fa49e9e4b9418011895ca2de6ed3f02c0
sergey <rusakovich_2000@mail.ru>
2026-03-10 14:46:38 +0300

    fix, add readme and requirements

--- test_models.py
+++ test_models.py
@@ -12,35 +12,35 @@ class TestPoint:
         assert str(new_point) == "Точка (1, 2)"
 
 class TestLine:
-    def test_create_point(self):
+    def test_create_line(self):
         new_line = Line(x=1, y=2, x_end=3, y_end=5)
         assert new_line.x == 1
         assert new_line.y == 2
         assert new_line.x_end == 3
         assert new_line.y_end == 5
     
-    def test_str_point(self):
+    def test_str_line(self):
         new_line = Line(x=1, y=2, x_end=3, y_end=5)
         assert str(new_line) == "Линия ((1, 2) (3, 5))"
 
 class TestCircle:
-    def test_create_point(self):
+    def test_create_circle(self):
         new_circle = Circle(x=1, y=2, radius=3)
         assert new_circle.x == 1
         assert new_circle.y == 2
         assert new_circle.radius == 3
     
-    def test_str_point(self):
+    def test_str_circle(self):
         new_circle = Circle(x=1, y=2, radius=3)
         assert str(new_circle) == "Круг (1, 2, 3)"
 
 class TestSquare:
-    def test_create_point(self):
+    def test_create_square(self):
         new_square = Square(x=1, y=2, side=3)
         assert new_square.x == 1
         assert new_square.y == 2
         assert new_square.side == 3
     
-    def test_str_point(self):
+    def test_str_square(self):
         new_square = Square(x=1, y=2, side=3)
         assert str(new_square) == "Квадрат (1, 2, 3)"
